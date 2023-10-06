0970d59e5b0d3cfe9f9b6ee19de105a59931de0e
Amir <amir.ghaffarie@gmail.com>
2023-10-06 22:35:08 +0330

    filter js changed

--- CardGameDB/static/base/js/filter.js
+++ CardGameDB/static/base/js/filter.js
@@ -1,12 +1,10 @@
 window.addEventListener("load", function() {
 (function($) {
   $(document).ready(function() {
+
     // Listen for changes to the field
-    $("id_group").on('change', function() {
-      // Perform your logic here
+    $("select[name='group']").change(function() {
       let newValue = $(this).val();
-      // Access the new field value and perform any desired actions
-      console.log('Field value changed to: ' + newValue);
     });
   });
 })(django.jQuery); })
